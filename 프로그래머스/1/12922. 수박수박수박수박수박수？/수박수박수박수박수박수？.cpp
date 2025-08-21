1f4abda3f4c4bcb63a8110ef98632ef54bb2d198
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2025-08-21 09:22:37 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    string s = "수박";
+    
+    for(int i = 0; i < n/2; i++){
+        answer += s;
+    }
+    if(n%2 == 1)
+        answer += "수";
+    
+    return answer;
+}
