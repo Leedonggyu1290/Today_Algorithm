993a71aab85b934c0593da52c51fbc6945b7cd35
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2025-08-21 09:32:35 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.68 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    int count = 1;
+    
+    for(int i = left; i <= right; i++){
+        for(int j = 1; j <= i/2; j++){
+            if(i%j == 0)
+                count++;
+        }
+        if(count % 2 == 0)
+            answer += i;
+        else
+            answer -= i;
+        
+        count = 1;
+    }
+    
+    return answer;
+}
