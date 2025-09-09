53bb5fbb0889d550419acebb23e565fdf4a399cb
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2025-09-09 09:10:24 +0900

    [level 1] Title: 과일 장수, Time: 30.82 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+    sort(score.begin(), score.end(), greater<>());
+    for(int i = 1; i <= score.size() / m; i++){
+        answer += score[i * m - 1] * m;
+    }
+    
+    return answer;
+}
