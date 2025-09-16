dc70cf31465e1eed686ecbb8ec13bb48752619f8
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2025-09-16 09:11:26 +0900

    [level 1] Title: 소수 만들기, Time: 0.77 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n){
+    for(int l = 2; l <= sqrt(n); l++){
+        if(n % l == 0){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    for(int i = 0; i < nums.size() - 2; i++){
+        for(int j = i + 1; j < nums.size() - 1; j++){
+            for(int k = j + 1; k < nums.size(); k++){
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if(isPrime(sum)){
+                    answer++;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
