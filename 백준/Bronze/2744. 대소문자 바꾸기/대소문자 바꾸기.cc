f525278e9d4ceecdbbb5743f37d6fbb7c197c57c
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2026-01-08 10:16:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+    string str; cin>>str;
+    for(auto it:str){
+        if(it>='a' && it<='z') cout << (char)(it-32);
+        else cout << (char)(it+32);
+    }
+    cout << '\n';
+}
