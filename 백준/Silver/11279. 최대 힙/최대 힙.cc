6d008ddcfd37d4adec16531738c2d5e41ff57df6
Nuel 이동규 <107760020+Leedonggyu1290@users.noreply.github.com>
2026-01-12 09:03:27 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include <utility>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+priority_queue<int> pq;
+
+int main() {
+	int N;
+	cin >> N;
+
+	while (N--) {
+		ios::sync_with_stdio(0); cin.tie(0);
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty())
+				cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
